a4b10e68ab3139119c47bc8dd211e90aa35a6a2d
Jawad Jahangir <jawadjahangir89@gmail.com>
2024-04-24 02:53:31 +0600

    all incoming changes merged

--- src/app/modules/machine/machine.routes.ts
+++ src/app/modules/machine/machine.routes.ts
@@ -20,24 +20,10 @@ router.patch(
   machineController.addSensorAttachedModuleInToMachine,
 );
 
-<<<<<<< HEAD
-// Start ------------------------------- XXXXX ----------------------------
-// those routers are for root user role routes
-router.post('/', machineController.createWashingMachine);
-router.get('/washing-machine/user', machineController.getMyWashingMachine);
-router.get('/general-machine/user', machineController.getMyGeneralMachine);
-router.get('/:id', machineController.getMachine);
-router.delete('/', machineController.deleteMachine);
-router.put('/status', machineController.changeStatus);
-router.put('/sensor', machineController.addSensor);
-// End --------------------------------- XXXXX ----------------------------
-=======
-
 // router.get('/washing-machine/user/:uid', machineController.getMyWashingMachine);
 // router.get('/general-machine/user/:uid', machineController.getMyGeneralMachine);
 // router.get('/:id', machineController.getMachine);
 // router.delete('/', machineController.deleteMachine);
 // router.put('/status', machineController.changeStatus);
 // router.put('/sensor', machineController.addSensor);
->>>>>>> 12ab0619eb4a71c040f35f8bdc58f4192d08d1d7
 export const machineRoutes = router;
