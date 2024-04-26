e7942c70d414ba0b43b761811ffeeac372a94705
KhalidMimMuzahid <khalidmimm@gmail.com>
2024-04-26 11:30:19 +0600

    create team api is successfully done

--- src/app/modules/teamOfEngineers/teamOfEngineers.service.ts
+++ src/app/modules/teamOfEngineers/teamOfEngineers.service.ts
@@ -1,12 +1,12 @@
-/* eslint-disable no-unused-vars */
+import { TeamOfEngineers } from './teamOfEngineers.model';
 import { Types } from 'mongoose';
 import { ServiceProviderBranchManager } from '../user/usersModule/branchManager/branchManager.model';
 import AppError from '../../errors/AppError';
 import httpStatus from 'http-status';
 import { ServiceProviderEngineer } from '../user/usersModule/serviceProviderEngineer/serviceProviderEngineer.model';
+import { TTeamOfEngineers } from './teamOfEngineers.interface';
 
 const makeTeamOfEngineerInToDB = async ({
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   teamName,
   serviceProviderEngineers,
   createdBy, // the user who sent this request; it an branch manager root user
@@ -15,10 +15,21 @@ const makeTeamOfEngineerInToDB = async ({
   serviceProviderEngineers: string[];
   createdBy: Types.ObjectId;
 }) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const branchManagerData = await ServiceProviderBranchManager.findOne({
     user: createdBy,
   });
+
+  const isExistTeamNameInSameBranch = await TeamOfEngineers.findOne({
+    serviceProviderBranch:
+      branchManagerData?.currentState?.serviceProviderBranch,
+    teamName,
+  });
+  if (isExistTeamNameInSameBranch) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      'Team name already exists in the same branch',
+    );
+  }
   let serviceProviderEngineers_id: Types.ObjectId[] = [];
 
   if (!serviceProviderEngineers?.length) {
@@ -37,7 +48,7 @@ const makeTeamOfEngineerInToDB = async ({
   }
 
   const serviceProviderEngineersData = await ServiceProviderEngineer.find({
-    user: { $in: serviceProviderEngineers_id },
+    _id: { $in: serviceProviderEngineers_id },
   });
   if (
     serviceProviderEngineers?.length !== serviceProviderEngineersData?.length
@@ -48,9 +59,42 @@ const makeTeamOfEngineerInToDB = async ({
     );
   }
 
-  console.log({ serviceProviderEngineersData });
+  let members = [];
+  try {
+    members = serviceProviderEngineersData?.map((each) => {
+      if (
+        each?.currentState?.serviceProviderCompany?.toString() !==
+          branchManagerData?.currentState?.serviceProviderCompany?.toString() ||
+        each?.currentState?.serviceProviderBranch?.toString() !==
+          branchManagerData?.currentState?.serviceProviderBranch?.toString()
+      ) {
+        throw Error;
+      }
 
-  return serviceProviderEngineersData; // for testing
+      return {
+        isDeleted: false,
+        member: each?._id,
+      };
+    });
+  } catch (error) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      'Some of engineers are not in your branch that you belongs to',
+    );
+  }
+
+  const teamOfEngineers: Partial<TTeamOfEngineers> = {};
+  teamOfEngineers.teamName = teamName;
+  teamOfEngineers.createdBy = createdBy;
+  teamOfEngineers.serviceProviderBranch =
+    branchManagerData?.currentState?.serviceProviderBranch;
+  teamOfEngineers.serviceProviderCompany =
+    branchManagerData?.currentState?.serviceProviderCompany;
+  teamOfEngineers.members = members;
+
+  const createdTeamOfEngineers = await TeamOfEngineers.create(teamOfEngineers);
+
+  return createdTeamOfEngineers; // for testing
 };
 export const teamOfEngineersServices = {
   makeTeamOfEngineerInToDB,
